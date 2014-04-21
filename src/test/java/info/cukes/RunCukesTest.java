b8d063eaa4523fb28bf786bb84cc723f42cc9d2e
Andy Glick <andyglick@gmail.com>
2014-04-21 12:32:33 -0400

    more simple changes

--- src/test/java/info/cukes/RunCukesTest.java
+++ src/test/java/info/cukes/RunCukesTest.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = {"info.cukes", "cucumber.runtime.java.spring.hooks"}, format = "pretty")
+@Cucumber.Options(glue = {"info.cukes", "cucumber.runtime.java.spring.hooks"},
+  format = {"html:target/cucumber-html-report"})
 public class RunCukesTest
 {
 }
