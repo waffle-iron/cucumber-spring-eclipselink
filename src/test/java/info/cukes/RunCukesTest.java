0aeaafca5db00c3647e47d0409242c9d9fb744e5
Andy Glick <andyglick@gmail.com>
2014-05-13 15:50:30 -0400

    updated RunCukesTest and replaced a deprecated feature with a supported one

--- src/test/java/info/cukes/RunCukesTest.java
+++ src/test/java/info/cukes/RunCukesTest.java
@@ -2,10 +2,11 @@ package info.cukes;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = {"info.cukes", "cucumber.runtime.java.spring.hooks"},
+@CucumberOptions(glue = {"info.cukes", "cucumber.runtime.java.spring.hooks"},
   format = {"html:target/cucumber-html-report"})
 public class RunCukesTest
 {
