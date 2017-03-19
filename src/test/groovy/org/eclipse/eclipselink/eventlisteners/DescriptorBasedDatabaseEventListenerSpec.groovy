04cf8c41c489a4d5496c628175b7734dd262197c
andyglick <andyglick@gmail.com>
2017-03-18 20:35:43 -0400

    committing all of the code currently in the source tree, adding a Spock test class, which has had all of its tests disabled

---
+++ src/test/groovy/org/eclipse/eclipselink/eventlisteners/DescriptorBasedDatabaseEventListenerSpec.groovy
@@ -0,0 +1,22 @@
+package org.eclipse.eclipselink.eventlisteners
+
+import org.eclipse.eclipselink.rs.eventListeners.DescriptorBasedDatabaseEventListener
+import org.eclipse.persistence.sessions.Session
+import spock.lang.Specification
+
+/**
+ * @author glick
+ */
+class DescriptorBasedDatabaseEventListenerSpec extends Specification {
+
+  def "exercise DescriptorBasedDatabaseEventListenerSpec for coverage"() {
+    given: "DescriptorBasedDatabaseEventListener class must be exercised for coverage"
+//    def eventListener = new DescriptorBasedDatabaseEventListener()
+//    def session = null
+
+    when: "the event listener is exercised"
+
+    then: ""
+//    eventListener.register()
+  }
+}
