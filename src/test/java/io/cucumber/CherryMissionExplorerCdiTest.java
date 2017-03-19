abb83264e5873096809d12106a8c73c890f928c7
andyglick <andyglick@gmail.com>
2017-03-18 20:31:08 -0400

    disabled the test class CherryMissionExplorerCdiTest

--- src/test/java/io/cucumber/CherryMissionExplorerCdiTest.java
+++ src/test/java/io/cucumber/CherryMissionExplorerCdiTest.java
@@ -28,7 +28,7 @@ import java.lang.invoke.MethodHandles;
 /**
  * @author glick
  */
-// @Ignore
+@Ignore
 public class CherryMissionExplorerCdiTest
 {
   private static final transient Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
