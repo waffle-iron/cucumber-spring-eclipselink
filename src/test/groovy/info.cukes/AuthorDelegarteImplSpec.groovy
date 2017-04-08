607609b42a8f5bd51e1272acff5651fb3031cdb0
andyglick <andyglick@gmail.com>
2017-04-08 10:03:19 -0400

    bumped versions, added members to .gitignore, added more Spock tests to raise coverage percentage

--- src/test/groovy/info.cukes/AuthorDelegarteImplSpec.groovy
+++ src/test/groovy/info.cukes/AuthorDelegarteImplSpec.groovy
@@ -14,4 +14,20 @@ class AuthorDelegarteImplSpec {
 
     authorDelegate.assignBookToAuthors(authors, book)
   }
+
+  def "compare author lists"() {
+
+    AuthorDelegate authorDelegate = new AuthorDelegateImpl()
+
+    List<Author> authors = Arrays.asList(new Author("Red Baron"))
+    List<Author> emptyOtherAuthors = Collections.emptyList()
+
+    authorDelegate.compareAuthorLists(authors, emptyOtherAuthors)
+
+    List<Author> otherAuthors = Arrays.asList(new Author("Snoopy"))
+    authorDelegate.compareAuthorLists(authors, otherAuthors)
+
+    otherAuthors = Arrays.asList(new Author("Red Baron"))
+    authorDelegate.compareAuthorLists(authors, otherAuthors)
+  }
 }
